036a4c0dd8a7211d8c95b208beda28e63642448f
SanderSaska <sander.saska@gmail.com>
2022-12-23 12:06:19 +0200

    Task 1 completed

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,18 +1,23 @@
 <template>
-<div>
-<h3> Courses  </h3>
-<table>
-          <tr>
-            <th>Code</th>
-            <th>Title</th>
-            <th>Semester</th>
-            <th>Credits</th>
-            <th>Description</th>
-          </tr>
-
-
-</table> 
-</div>
+  <div>
+    <h3>Courses</h3>
+    <table>
+      <tr>
+        <th>Code</th>
+        <th>Title</th>
+        <th>Semester</th>
+        <th>Credits</th>
+        <th>Description</th>
+      </tr>
+      <tr class="item" v-for="course in courses" :key="course.id">
+        <td>{{ course.code }}</td>
+        <td>{{ course.title }}</td>
+        <td>{{ course.semester }}</td>
+        <td>{{ course.credits }}</td>
+        <td>{{ course.description }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -29,15 +34,13 @@ export default {
         .then((response) => response.json())
         .then((data) => (this.courses = data))
         .catch((err) => console.log(err.message));
-   },
+    },
   },
   mounted() {
     this.fetchCourses();
     console.log("mounted");
-  } 
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
